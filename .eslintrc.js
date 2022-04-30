de165b594397b848b282f7cf005b11ab783ed1ff
wudexiong <1245295089@qq.com>
2022-04-30 17:51:31 +0800

    init :tada: 基础开发环境搭建完毕

---
+++ .eslintrc.js
@@ -0,0 +1,23 @@
+module.exports = {
+    "env": {
+        "es2021": true,
+        "node": true
+    },
+    "extends": [
+        "eslint:recommended",
+        "plugin:@typescript-eslint/recommended",
+        // 解决vuter prettier eslint格式冲突
+        'standard',
+        'plugin:prettier/recommended' 
+    ],
+    "parser": "@typescript-eslint/parser",
+    "parserOptions": {
+        "ecmaVersion": "latest",
+        "sourceType": "module"
+    },
+    "plugins": [
+        "@typescript-eslint"
+    ],
+    "rules": {
+    }
+}
---
+++ src/index.ts
@@ -0,0 +1,12 @@
+/*
+ * @Author: WuDeXiong
+ * @Date: 2022-04-30 16:27:39
+ * @LastEditors: OBKoro1
+ * @LastEditTime: 2022-04-30 16:49:56
+ * @FilePath: \pdp-cli\src\index.ts
+ * @Description: 项目入口文件
+ * 
+ * Copyright (c) 2022 by WuDeXiong, All Rights Reserved. 
+ */
+
+
